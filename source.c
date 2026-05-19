f593b4e5251b738d5a99511dee5aab5559580e3a
vinix38 <vinceguichard04@gmail.com>
2026-05-19 11:16:30 +0200

    support for float (no implicit)

--- source.c
+++ source.c
@@ -1,9 +1,10 @@
-main(int x, int y, int z){
-    z = 0;
+main(int x, int y, double z){
+    z = 1.0;
     x = 1;
     while(x){
-        z = z + 1 * 2;
+        z = z * 2.0;
         x = x - 1;
     }
+    print(z);
     return y;
 }
