95e26afcaf0a1180496a181d2257d45a08b72397
vinix38 <vinceguichard04@gmail.com>
2026-05-26 18:07:21 +0200

    better asm

--- squelette.asm
+++ squelette.asm
@@ -1,10 +1,17 @@
+default rel
+
 extern printf, atoi, atof
 section .data
 argv: dq 0
 format_entier: db "%lld", 0
 format_flottant: db "%lf", 0
+format_retour: db 10, 0 ; 10="\n", 0="\0"
 
 DECL_VARS
+
+section .rodata
+CONSTANTES
+
 global main
 section .text
 main:
@@ -13,6 +20,11 @@ mov rbp, rsp
 mov [argv], rsi
 INIT_VARS
 COMMAND 
+
+mov rdi, format_retour
+xor rax, rax
+call printf
+
 RETURN
 pop rbp
 ret
