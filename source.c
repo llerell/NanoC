0a81096d65a8cd852e7249f0f0de9be2e8e55286
Owen LE RAY <leray.owen@gmail.com>
2026-05-26 11:09:28 +0200

    Added dicts

--- source.c
+++ source.c
@@ -1,10 +1,25 @@
-main(dict dico<int, int>, int x){
-    x = 0;
-    dico = {0: 1, 1: 2, 2: 3};
-    dico[0] = 4;
-    del dico[1];
-    foreach (k in dico) {
-        x = x + dico[k];
+main(int argument, dict mon_dict<int,int>,
+    int cle_courante,
+    int valeur_courante,
+    int k, 
+    int v) {
+
+    v = 0;
+    mon_dict = {10: 100, 20: 200, 30: 300};
+    
+    mon_dict[40] = 400;
+    mon_dict[20] = 222;
+    
+    del mon_dict[30];
+    
+    print(valeur_courante);
+    
+    foreach(cle_courante in mon_dict) {
+        valeur_courante = mon_dict[cle_courante];
+        print(valeur_courante);
+        v = v + valeur_courante;
+        print(v);
     }
-    return dico;
+    
+    return 0;
 }
