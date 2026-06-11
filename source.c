1ca3726f92964aef49a3ce9b337aa91ef3976c43
vinix38 <vinceguichard04@gmail.com>
2026-06-12 00:10:06 +0200

    some testing

--- source.c
+++ source.c
@@ -5,15 +5,21 @@ main() {
     int n = 0;
     dict<int,int> mon_dict = {10: 100, 20: 200, 30: 300};
     
-    mon_dict[40] = 400;
-    mon_dict[20] = 222;
+    mon_dict[40] = 400 + 3 / 20 - 8 + 2 * 2;
+    mon_dict[20] = !222 & 1 | 2;
 
     print(mon_dict[20]);
     print(mon_dict[30]);
     print(mon_dict[40]);
+
+    dict<int,dict<int,int>> dico2 = {2: {10:100}};
+
         
-    // dico2[0] = {10:100};
-    // dico2[1] = mon_dict;
+    dico2[0] = {10:100};
+    dico2[1] = mon_dict;
+
+    //print(dico2[1][20]);
+    
     
     // foreach(cle_courante in mon_dict) {
     //     valeur_courante = mon_dict[cle_courante];
